680dbcf4d42153a26778eed4432cc853f322d5f1
Alexey Nadtochiy <nadtochiyalex@mail.ru>
2021-09-06 12:52:39 +0300

    Init commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/aleksmvp/logmiddleware
+
+go 1.14
+
+require (
+	github.com/aleksmvp/logger v0.1.2
+	github.com/prometheus/client_golang v1.11.0
+)
---
+++ log.go
@@ -0,0 +1,64 @@
+package middleware
+
+import (
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/aleksmvp/logger"
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+type AccessLogMiddleware struct {
+	Logger logger.ILogger
+}
+
+type statusRecorder struct {
+	http.ResponseWriter
+	status int
+}
+
+var (
+	hits = prometheus.NewCounterVec(prometheus.CounterOpts{
+		Name: "hits",
+	}, []string{"status", "path", "method"})
+
+	timings = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name: "method_timings",
+		Help: "Per method timing",
+	}, []string{"method"})
+)
+
+func (rec *statusRecorder) WriteHeader(code int) {
+	rec.status = code
+	rec.ResponseWriter.WriteHeader(code)
+}
+
+func NewAccessLogMiddleware(logger logger.ILogger) AccessLogMiddleware {
+	err := prometheus.Register(hits)
+	if err != nil {
+		logger.LogError("middleware", "NewAccessLogMiddleware", err)
+	}
+	err = prometheus.Register(timings)
+	if err != nil {
+		logger.LogError("middleware", "NewAccessLogMiddleware", err)
+	}
+	return AccessLogMiddleware{Logger: logger}
+}
+
+func (m AccessLogMiddleware) Log(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		rec := statusRecorder{w, 200}
+		next.ServeHTTP(&rec, r)
+
+		url := strings.Split(r.URL.String(), "?")[0]
+
+		hits.WithLabelValues(strconv.Itoa(rec.status), url, r.Method).Inc()
+		timings.WithLabelValues(r.URL.String()).Observe(time.Since(start).Seconds())
+
+		m.Logger.LogAccess(r, rec.status, time.Since(start))
+	})
+}
